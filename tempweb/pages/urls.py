4fc0c897d2bd90bb66da91d492c5be3bc86de526
caser <itscaseyr@gmail.com>
2019-10-29 21:33:09 -0400

    Rework on html for future work

--- tempweb/pages/urls.py
+++ tempweb/pages/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     #pattern as follows path('subpage', views function, name)
     path('', views.home, name='pages-home'),
+    path('about', views.about, name='pages-about'),
 ]
